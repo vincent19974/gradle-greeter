d35cbf1e112723bd359f50c167a819a2d3e77a3b
vincent19974 <nambatac.vince@gmail.com>
2020-12-09 16:38:18 +0800

    Initial commit

---
+++ src/main/java/Hello/HelloWorld.java
@@ -0,0 +1,8 @@
+package Hello;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.sayHello());
+    }
+}
